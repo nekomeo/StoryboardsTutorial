1c346a560a30204b8fe77ca6a070ee4ba7631db0
nekomeo <linh.tu545@gmail.com>
2017-06-12 23:53:20 -0700

    Initial Commit

---
+++ Ratings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ratings
+//
+//  Created by Elle Ti on 2017-06-12.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ratings/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Ratings
+//
+//  Created by Elle Ti on 2017-06-12.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
